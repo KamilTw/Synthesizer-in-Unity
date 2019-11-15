bba73715db51e1e1dd44b71c633dfe06b2f39ae2
Kamil <33379395+Rivi96@users.noreply.github.com>
2019-11-15 20:08:07 +0100

    Envelope time fix

--- Synthesizer/Assets/Scripts/Oscillator.cs
+++ Synthesizer/Assets/Scripts/Oscillator.cs
@@ -14,8 +14,7 @@ public class Oscillator : MonoBehaviour
 
     private double samplingFrequency = 48000.0;
     private double phase;
-    public float time;
-    private int t = 0;
+    private float time = 0;
     private Envelope envelope;
     private System.Random rand = new System.Random();
     private int redNoiseSampleCounter = 50;
@@ -24,19 +23,14 @@ public class Oscillator : MonoBehaviour
 
     private int skippedSamples = 15;
 
-    void Update()
-    {
-        time = Time.time;
-    }
-
     public void NoteOn()
     {
-        envelope.NoteOn(time);
+        envelope.NoteOn(time / samplingFrequency);
     }
 
     public void NoteOff()
     {
-        envelope.NoteOff(time);
+        envelope.NoteOff(time / samplingFrequency);
     }
 
     public void SetEnvelope(Envelope envelope)
@@ -48,11 +42,11 @@ public class Oscillator : MonoBehaviour
     {
         float data;
         double increment;
-        t++;
+        time++;
 
         increment = frequency * 2.0 * Mathf.PI / samplingFrequency;
         phase += increment;
-        //phase = increment * t;
+        //phase = increment * time;
         
         data = GenerateWave(phase, lfoFreq, lfoAmpSlider);
 
@@ -65,7 +59,7 @@ public class Oscillator : MonoBehaviour
         int activeOscCounter = 0;
 
         // Calculated sin (with LFO modulation if lfoFreq and lfoAmpSlider are not zeros)
-        float calculatedSin = Mathf.Sin((float)phase + lfoAmpSlider * (float)frequency * Mathf.Sin(lfoFreq * t));
+        float calculatedSin = Mathf.Sin((float)phase + lfoAmpSlider * (float)frequency * Mathf.Sin(lfoFreq * time));
 
         if (sinGain > 0.0f)
         {
@@ -97,7 +91,7 @@ public class Oscillator : MonoBehaviour
             float d = 0;
             for (int n = 1; n < 20; n++)
             {
-                d += (Mathf.Sin(n * (float)phase + lfoAmpSlider * (float)frequency * Mathf.Sin(lfoFreq * t))) / n;
+                d += (Mathf.Sin(n * (float)phase + lfoAmpSlider * (float)frequency * Mathf.Sin(lfoFreq * time))) / n;
             }
             data += d * sawtoothGain * (2.0f / Mathf.PI);
             activeOscCounter++;
@@ -110,7 +104,7 @@ public class Oscillator : MonoBehaviour
             }
             else
             {
-                data += whiteNoiseGain * Mathf.Sin((float)(rand.NextDouble() * 2.0 - 1.0) + lfoAmpSlider * (float)frequency * Mathf.Sin(lfoFreq * t));
+                data += whiteNoiseGain * Mathf.Sin((float)(rand.NextDouble() * 2.0 - 1.0) + lfoAmpSlider * (float)frequency * Mathf.Sin(lfoFreq * time));
             }
             
             activeOscCounter++;
@@ -133,7 +127,7 @@ public class Oscillator : MonoBehaviour
                 }
                 else
                 {
-                    redNoiseLastSample = Mathf.Sin((float)(rand.NextDouble() * 2.0 - 1.0) + lfoAmpSlider * (float)frequency * Mathf.Sin(lfoFreq * t));
+                    redNoiseLastSample = Mathf.Sin((float)(rand.NextDouble() * 2.0 - 1.0) + lfoAmpSlider * (float)frequency * Mathf.Sin(lfoFreq * time));
                 }
                 
                 redNoiseSampleCounter = 2;
@@ -145,6 +139,6 @@ public class Oscillator : MonoBehaviour
             activeOscCounter++;
         }
 
-        return (float)envelope.GetAmplitude(time) * data / activeOscCounter;
+        return (float)envelope.GetAmplitude(time / samplingFrequency) * data / activeOscCounter;
     }
 }
